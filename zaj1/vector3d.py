8c2a3d6ee6fb5d4feca8fa9c962b0afd0f8549cb
Michał Ślifierski <m.slifierski@gmail.com>
2024-03-02 11:24:47 +0100

    feature/vector3D getters and setters

--- zaj1/vector3d.py
+++ zaj1/vector3d.py
@@ -4,11 +4,31 @@ class Vector3D:
         self.y = y
         self.z = z
 
+    def get_x(self): 
+        return self.x 
+
+    def set_x(self, x): 
+        self.x = x
+
+    def get_y(self): 
+        return self.y 
+
+    def set_y(self, y): 
+        self.y = y 
+    
+    def get_z(self): 
+        return self.z 
+
+    def set_z(self, z): 
+        self.z = z 
+
     def __str__(self):
         return f"x: {self.x}, y: {self.y}, z: {self.z}"
 
     def pokaz_informacje(self):
         print(f"x: {self.x}, y: {self.y}, z: {self.z}")
+
+
 
 
 # Utworzenie obiektu klasy Vector3D
